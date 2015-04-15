25804bd7c0bfcedc5489561fe0994d062f00bd35
Leonard Martin <leonard.martin@digital.cabinet-office.gov.uk>
2015-04-15 11:21:37 +0100

    Reset request object properties to empty
    Because objects are passed by reference then if properties are set manually on these objects then they are set to the defaults and subsequent calls will include the same properties. Reset all the object properties of defaults to empty on each call.

--- test/spec.req.js
+++ test/spec.req.js
@@ -1,6 +1,6 @@
 var reqres = require('../');
 
-describe('res', function () {
+describe('req', function () {
 
     var req;
 
@@ -13,6 +13,35 @@ describe('res', function () {
         it('is constructed from url and baseUrl properties', function () {
             reqres.req({ url: '/foo' }).originalUrl.should.equal('/foo');
             reqres.req({ url: '/foo', baseUrl: '/base' }).originalUrl.should.equal('/base/foo');
+        });
+
+    });
+
+    describe('object properties are reset', function () {
+
+        it('params', function () {
+            req.params.foo = 'bar';
+            reqres.req({}).params.should.eql({});
+        });
+
+        it('body', function () {
+            req.body.foo = 'bar';
+            reqres.req({}).body.should.eql({});
+        });
+
+        it('session', function () {
+            req.session.foo = 'bar';
+            reqres.req({}).session.should.eql({});
+        });
+
+        it('cookies', function () {
+            req.cookies.foo = 'bar';
+            reqres.req({}).cookies.should.eql({});
+        });
+
+        it('query', function () {
+            req.query.foo = 'bar';
+            reqres.req({}).query.should.eql({});
         });
 
     });
